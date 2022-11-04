41c5e865fc9c4a0bf3eaaab2f30e792d33e65dc8
Hiren95 <hiren@topsinfosolutions.com>
2022-11-04 14:59:47 +0530

    Display the list of BLE Devices, connect peripheral, show details of device

---
+++ BLE/ViewControllers/CharacteristicsDetailVC.swift
@@ -0,0 +1,279 @@
+//
+//  CharacteristicsDetailVC.swift
+//  BLEScanner
+//
+//  Created by Tops on 17/03/20.
+//  Copyright © 2020 GG. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+
+class CharacteristicsDetailVC: UIViewController {
+    
+    var peripheral: CBPeripheral!
+    var charecter : [CBCharacteristic] = []
+    
+    var readChar : CBCharacteristic!
+    var writeChar : CBCharacteristic!
+    
+    @IBOutlet weak var tv_DeatilVC : UITextView!
+    @IBOutlet weak var btn_read : UIButton!
+    @IBOutlet weak var btn_write : UIButton!
+    
+    var timer : Timer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        peripheral.delegate = self
+        self.btn_read.isHidden = true
+        self.btn_write.isHidden = true
+        
+        self.navigationController?.navigationBar.backgroundColor = .blue
+
+    }
+    
+
+    override func viewWillAppear(_ animated: Bool) {
+        
+        for i in charecter {
+            
+            if i.uuid.uuidString == "fe462baf-0f2f-427d-ab7e-5277e35e7bcf" {
+                readChar = i
+                 timer = Timer.scheduledTimer(timeInterval: 0.25, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
+            }
+            if i.uuid.uuidString == "cb0e65ee-2ca3-4022-a55b-84dd7f6cd114"{
+               writeChar = i
+            }
+        }
+    }
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        if let time = self.timer {
+            time.invalidate()
+        }
+    }
+    
+    // Created timer to read device values
+    @objc func fireTimer() {
+        
+        if let reader = readChar {
+            peripheral.readValue(for: readChar)
+            print("Timer fired!")
+        }
+        
+    }
+    // Button to read data from device
+    @IBAction func readData(_ sender:UIButton) {
+        
+        if let reader = readChar {
+            peripheral.readValue(for: readChar)
+        }
+    }
+    // To clear data
+    @IBAction func clearData(_ sender:UIButton) {
+        
+        if writeChar == nil {
+            return
+        }
+        
+        if writeChar.uuid.uuidString == "cb0e65ee-2ca3-4022-a55b-84dd7f6cd114" {
+            if writeChar.properties.contains(CBCharacteristicProperties.write) {
+                var parameter = UInt8(0)//NSInteger(1)
+                let data = NSData(bytes: &parameter, length: 1)
+                peripheral.writeValue(data as Data, for: writeChar, type: .withResponse)
+                return
+            }
+            
+            if writeChar.properties.contains(CBCharacteristicProperties.writeWithoutResponse) {
+                var parameter = UInt8(0)
+                let data = NSData(bytes: &parameter, length: 1)
+                peripheral.writeValue(data as Data, for: writeChar, type: .withoutResponse)
+                return
+            }
+        }
+    }
+    // to write data  from device
+    @IBAction func writeData(_ sender:UIButton) {
+        
+        if writeChar.uuid.uuidString == "cb0e65ee-2ca3-4022-a55b-84dd7f6cd114" {
+            if writeChar.properties.contains(CBCharacteristicProperties.write) {
+                var parameter = UInt8(1)//NSInteger(1)
+                let data = NSData(bytes: &parameter, length: 1)
+                peripheral.writeValue(data as Data, for: writeChar, type: .withResponse)
+                return
+            }
+            
+            if writeChar.properties.contains(CBCharacteristicProperties.writeWithoutResponse) {
+                var parameter = UInt8(1)
+                let data = NSData(bytes: &parameter, length: 1)
+                peripheral.writeValue(data as Data, for: writeChar, type: .withoutResponse)
+                return
+            }
+        }else{
+            if writeChar.properties.contains(CBCharacteristicProperties.write) {
+                var parameter = NSInteger(1)
+                let data = NSData(bytes: &parameter, length: 1)
+                peripheral.writeValue(data as Data, for: writeChar, type: .withResponse)
+                return
+            }
+            
+            if writeChar.properties.contains(CBCharacteristicProperties.writeWithoutResponse) {
+                var parameter = NSInteger(1)
+                let data = NSData(bytes: &parameter, length: 1)
+                peripheral.writeValue(data as Data, for: writeChar, type: .withoutResponse)
+                return
+            }
+        }
+    }
+    
+}
+//MARK: CBPeripheral delegate method
+
+
+extension CharacteristicsDetailVC : CBPeripheralDelegate{
+    
+    // MARK: - CBPeripheralDelegate
+    
+    func peripheral(_ peripheral: CBPeripheral, didWriteValueFor characteristic: CBCharacteristic, error: Error?) {
+        if let e = error {
+            print("ERROR didUpdateValue \(e)")
+            let str = "ERROR didUpdateValue \(e)"
+            self.tv_DeatilVC.text = str
+            return
+        }
+        guard let data = characteristic.value else { return }
+        
+        
+        let struuu = self.tv_DeatilVC.text!
+        if data.hexEncodedString().count >= 8 {
+            
+            //Get Count From Hex
+            let countHex = data.hexEncodedString().prefix(4)
+            let countPref = countHex.prefix(2)
+            let countSuf = countHex.suffix(2)
+            let countSwipedData = countSuf + countPref
+            
+            var Count = ""
+            if let value = UInt16(countSwipedData, radix: 16) {
+                print(value)
+                Count = "\(value)"
+            } else if let value = Int(countSwipedData, radix: 16) {
+                print(value)
+                Count = "\(value)"
+            } else if let value = String.init(data:data, encoding: .utf8) {
+                Count = "\(value)"
+            }
+  
+            // Get RPM from Hex
+            let RPMHex = data.hexEncodedString().suffix(4)
+            let RMPPref = RPMHex.prefix(2)
+            let RPMSuf = RPMHex.suffix(2)
+            let RPMSwipedData = RPMSuf + RMPPref
+            
+            var RPM = ""
+            if let value = UInt16(RPMSwipedData, radix: 16) {
+                print(value)
+                RPM = "\(value)"
+            } else if let value = Int(RPMSwipedData, radix: 16) {
+                print(value)
+                RPM = "\(value)"
+            } else if let value = String.init(data:data, encoding: .utf8) {
+                RPM = "\(value)"
+            }
+            self.tv_DeatilVC.text = "Count : \(Count)\nRPM : \(RPM)"
+        } else {
+            
+             var str = ""
+            if let value = UInt16(data.hexEncodedString(), radix: 16) {
+                print(value)
+                str = "\(value)"
+            } else if let value = Int(data.hexEncodedString(), radix: 16) {
+                print(value)
+                str = "\(value)"
+            } else if let value = String.init(data:data, encoding: .utf8) {
+                str = "\(value)"
+            }
+            self.tv_DeatilVC.text = "Written value : \(str)"
+        }
+ 
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        if let e = error {
+            print("ERROR didUpdateValue \(e)")
+            let str = "ERROR didUpdateValue \(e)"
+            self.tv_DeatilVC.text = str
+            return
+        }
+        guard let data = characteristic.value else { return }
+        
+        let struuu = self.tv_DeatilVC.text!
+        if data.hexEncodedString().count >= 8 {
+            
+            //Get Count From Hex
+            let countHex = data.hexEncodedString().prefix(4)
+            let countPref = countHex.prefix(2)
+            let countSuf = countHex.suffix(2)
+            let countSwipedData = countSuf + countPref
+            
+            var Count = ""
+            if let value = UInt16(countSwipedData, radix: 16) {
+                print(value)
+                Count = "\(value)"
+            } else if let value = Int(countSwipedData, radix: 16) {
+                print(value)
+                Count = "\(value)"
+            } else if let value = String.init(data:data, encoding: .utf8) {
+                Count = "\(value)"
+            }
+            
+            let RPMHex = data.hexEncodedString().suffix(4)
+            let RMPPref = RPMHex.prefix(2)
+            let RPMSuf = RPMHex.suffix(2)
+            let RPMSwipedData = RPMSuf + RMPPref
+            
+            var RPM = ""
+            if let value = UInt16(RPMSwipedData, radix: 16) {
+                print(value)
+                RPM = "\(value)"
+            } else if let value = Int(RPMSwipedData, radix: 16) {
+                print(value)
+                RPM = "\(value)"
+            } else if let value = String.init(data:data, encoding: .utf8) {
+                RPM = "\(value)"
+            }
+            
+            self.tv_DeatilVC.text = "Count : \(Count)\n\nRPM : \(RPM)"
+        }
+    }
+    
+}
+// MARK: data conversion
+extension Data {
+    
+    init<T>(from value: T) {
+        self = Swift.withUnsafeBytes(of: value) { Data($0) }
+    }
+    
+    func to<T>(type: T.Type) -> T? where T: ExpressibleByIntegerLiteral {
+        var value: T = 0
+        guard count >= MemoryLayout.size(ofValue: value) else { return nil }
+        _ = Swift.withUnsafeMutableBytes(of: &value, { copyBytes(to: $0)} )
+        return value
+    }
+}
+
+
+extension Data {
+    struct HexEncodingOptions: OptionSet {
+        let rawValue: Int
+        static let upperCase = HexEncodingOptions(rawValue: 1 << 0)
+    }
+    
+    func hexEncodedString(options: HexEncodingOptions = []) -> String {
+        let format = options.contains(.upperCase) ? "%02hhX" : "%02hhx"
+        return map { String(format: format, $0) }.joined()
+    }
+}
---
+++ BLE/ViewControllers/CharacteriticVC.swift
@@ -0,0 +1,110 @@
+//
+//  CharacteriticVC.swift
+//  BLEScanner
+//
+//  Created by Tops on 17/03/20.
+//  Copyright © 2020 GG. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+
+class CharacteriticVC: UIViewController {
+
+    @IBOutlet weak var tbl_Characteristic : UITableView!
+
+    var service : CBService!
+    var peripheral: CBPeripheral!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tbl_Characteristic.delegate = self
+        tbl_Characteristic.dataSource = self
+        tbl_Characteristic.reloadData()
+        
+        self.navigationController?.navigationBar.backgroundColor = .blue
+
+        // Do any additional setup after loading the view.
+    }
+}
+
+
+// MARK: tableview datasource and delegate methods
+extension CharacteriticVC : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if let service = self.service {
+            
+            if let chare = service.characteristics {
+                 return chare.count
+            }
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "characteristicCell") as! characteristicCell
+        
+        
+        
+        if let item = service.characteristics?[indexPath.row] {
+            print("--------------------------------------------")
+            
+            cell.lbl_Primary.text = "characteristics \((indexPath.row))"
+            
+            
+            print("Characteristic UUID: \(item.uuid)")
+            print("Characteristic isNotifying: \(item.isNotifying)")
+            print("Characteristic properties: \(item.properties)")
+            print("Characteristic descriptors: \(String(describing: item.descriptors))")
+            print("Characteristic value: \(String(describing: item.value))")
+            var permission = String()
+            
+            if item.properties.contains(CBCharacteristicProperties.read) {
+               permission.append("Read,")
+            }
+            if item.properties.contains(CBCharacteristicProperties.write) {
+                permission.append("Write,")
+            }
+            if item.properties.contains(CBCharacteristicProperties.writeWithoutResponse) {
+                permission.append("Write No Response,")
+            }
+            if item.properties.contains(CBCharacteristicProperties.notify){
+                permission.append("Notify,")
+            }
+            if item.properties.contains(CBCharacteristicProperties.indicate){
+                permission.append("Indicate,")
+            }
+            cell.lbl_Secondary.text = "\(item.uuid) \n Type : \(permission)"
+    
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let item = service.characteristics?[indexPath.row] {
+            
+           let vc = self.storyboard?.instantiateViewController(withIdentifier: "CharacteristicsDetailVC") as! CharacteristicsDetailVC
+            vc.charecter = service.characteristics!
+            vc.peripheral = self.peripheral
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+}
+
+class characteristicCell : UITableViewCell {
+    
+    @IBOutlet weak var lbl_Primary : UILabel!
+    @IBOutlet weak var lbl_Secondary : UILabel!
+}
+
+
+
